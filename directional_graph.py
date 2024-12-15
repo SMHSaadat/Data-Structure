fc5398e2124839d2f589061fce78ab084dcb82a6
SMHSaadat <hasanmicrob@gmail.com>
2024-12-15 12:39:10 +0330

    rewrite directional graph

--- directional_graph.py
+++ directional_graph.py
@@ -1,21 +1,25 @@
-class Graph:
+class DGraph:
     def __init__(self, vertices):
+
         self.vertices = vertices
         self.adj_matrix = [[0] * vertices for _ in range(vertices)]
 
-    def add_edge(self, u, v, weight=1):
+    def add_edge(self, u, v):
+
         if 0 <= u < self.vertices and 0 <= v < self.vertices:
-            self.adj_matrix[u][v] = weight
+            self.adj_matrix[u][v] = 1
         else:
             raise ValueError("Vertex index out of bounds")
 
     def remove_edge(self, u, v):
+
         if 0 <= u < self.vertices and 0 <= v < self.vertices:
             self.adj_matrix[u][v] = 0
         else:
             raise ValueError("Vertex index out of bounds")
 
     def bfs(self, start):
+
         if not (0 <= start < self.vertices):
             raise ValueError("Start vertex index out of bounds")
 
@@ -60,16 +64,16 @@ class Graph:
         return dfs_order
 
     def display(self):
-
         for row in self.adj_matrix:
             print(" ".join(map(str, row)))
 
+# Example Usage
 g = Graph(5)
-g.add_edge(0, 1, 2)
-g.add_edge(0, 2, 5)
-g.add_edge(1, 3, 3)
-g.add_edge(3, 4, 4)
-g.add_edge(4, 0, 1)
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 3)
+g.add_edge(3, 4)
+g.add_edge(4, 0)
 g.display()
 
 print("BFS starting from vertex 0:", g.bfs(0))
