c35474bcc85243171cd97bcf288bd3e990e07049
SMHSaadat <hasanmicrob@gmail.com>
2024-12-15 12:35:37 +0330

    change code in dorectionaln graph

--- directional_graph.py
+++ directional_graph.py
@@ -1,28 +1,76 @@
-class WeightedGraph:
+class Graph:
     def __init__(self, vertices):
         self.vertices = vertices
-        self.adj_matrix = [[float('inf')] * vertices for _ in range(vertices)]
-        for i in range(vertices):
-            self.adj_matrix[i][i] = 0  # Distance to itself is zero
+        self.adj_matrix = [[0] * vertices for _ in range(vertices)]
 
-    def add_edge(self, u, v, weight):
-
+    def add_edge(self, u, v, weight=1):
         if 0 <= u < self.vertices and 0 <= v < self.vertices:
             self.adj_matrix[u][v] = weight
-            self.adj_matrix[v][u] = weight  # For undirected graph
         else:
             raise ValueError("Vertex index out of bounds")
+
+    def remove_edge(self, u, v):
+        if 0 <= u < self.vertices and 0 <= v < self.vertices:
+            self.adj_matrix[u][v] = 0
+        else:
+            raise ValueError("Vertex index out of bounds")
+
+    def bfs(self, start):
+        if not (0 <= start < self.vertices):
+            raise ValueError("Start vertex index out of bounds")
+
+        visited = [False] * self.vertices
+        queue = [start]
+        bfs_order = []
+
+        visited[start] = True
+
+        while queue:
+            current = queue.pop(0)
+            bfs_order.append(current)
+
+            for neighbor in range(self.vertices):
+                if self.adj_matrix[current][neighbor] != 0 and not visited[neighbor]:
+                    visited[neighbor] = True
+                    queue.append(neighbor)
+
+        return bfs_order
+
+    def dfs(self, start):
+
+        if not (0 <= start < self.vertices):
+            raise ValueError("Start vertex index out of bounds")
+
+        visited = [False] * self.vertices
+        stack = [start]
+        dfs_order = []
+
+        while stack:
+            current = stack.pop()
+
+            if not visited[current]:
+                visited[current] = True
+                dfs_order.append(current)
+
+                # Push neighbors to the stack in reverse order to maintain order
+                for neighbor in range(self.vertices - 1, -1, -1):
+                    if self.adj_matrix[current][neighbor] != 0 and not visited[neighbor]:
+                        stack.append(neighbor)
+
+        return dfs_order
 
     def display(self):
 
         for row in self.adj_matrix:
-            print(" ".join(map(lambda x: str(x) if x != float('inf') else 'inf', row)))
+            print(" ".join(map(str, row)))
 
-g = WeightedGraph(6)
-g.add_edge(0, 1, 4)
-g.add_edge(0, 4, 2)
-g.add_edge(1, 3, 5)
-g.add_edge(2, 4, 3)
-g.add_edge(3, 4, 6)
-g.add_edge(4, 5, 1)
+g = Graph(5)
+g.add_edge(0, 1, 2)
+g.add_edge(0, 2, 5)
+g.add_edge(1, 3, 3)
+g.add_edge(3, 4, 4)
+g.add_edge(4, 0, 1)
 g.display()
+
+print("BFS starting from vertex 0:", g.bfs(0))
+print("DFS starting from vertex 0:", g.dfs(0))
